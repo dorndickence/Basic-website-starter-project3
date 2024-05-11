36f0ac08ae93fd582d1a6a80a31520b58310005c
Dornavan <130479724+dorndickence@users.noreply.github.com>
2024-05-11 08:52:16 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const drop = document.getElementById("dropdownclick");
+function dropMeDown (){
+if (drop.className==="topnav"){
+    drop.className +=" responsive";
+
+} else {
+     drop.className ="topnav";
+
+   }
+  }
+
+
